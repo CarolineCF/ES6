951af1f33635f7a81df82d34f76df9faa56fd802
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-24 16:12:29 +0200

    Destructing terminus

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -3,11 +3,18 @@
 const [a, b] = [1, 2, 3, 4]
 console.log(a)
 console.log(b)
+// dans un tableau, on appel la position de l'élément
 
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35}
+const {first, last, age} = {first: 'Paul', last: 'Henta', age: 35}
 console.log(first)
 console.log(last)
+console.log(age)
+//dans un objet on appel le nom de l'élément
 
 //4.2 Application
 
-console.log(data)
+Object.values(data).forEach(value => {
+    
+    console.log(value.texte + ' date de début: '+value.dateDebut +' date de fin: '+ value.dateFin);
+});
+
