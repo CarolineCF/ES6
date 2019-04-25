cd4176956931d8d694ce5a2488fbc83b0d753903
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-25 09:52:17 +0200

    import/export terminus

--- 5-import/script.js
+++ 5-import/script.js
@@ -3,6 +3,16 @@
 import {hello} from './functions.js'
 
 hello()
+import {showError} from './functions.js'
 
+showError('bug')
+import {default as cat} from './functions.js'
+cat()
 // 5-2 Export
+import * as cal from './math.js'
 const [a, b] = [7, 9]
+console.log(cal.sum(a,b))
+console.log(cal.sub(a,b))
+console.log(cal.multiply(a,b))
+console.log(cal.divide(a,b))
+console.log(cal.pow(a,b))
