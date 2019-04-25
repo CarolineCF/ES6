54f365f4924fd3bad067025071cc34e1ff9cb34d
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-25 09:20:33 +0200

    destructuring modified

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -12,9 +12,13 @@ console.log(age)
 //dans un objet on appel le nom de l'élément
 
 //4.2 Application
+for (let element in data) {
+    const { dateDebut, texte, dateFin } = data[element]
+    console.log(dateDebut, texte, dateFin)
+}
 
-Object.values(data).forEach(value => {
+/*Object.values(data).forEach(value => {
     
     console.log(value.texte + ' date de début: '+value.dateDebut +' date de fin: '+ value.dateFin);
-});
+}); */
 
